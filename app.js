92b8f9b853fd6122e33fdcfee90692448fd17a30
Nandagopal0812 <Gopalnanda701@gmail.com>
2024-10-03 13:18:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// // script.js     `
+
+// document.getElementById('search-box').addEventListener('input', function() {
+//     let filter = this.value.toLowerCase();
+//     let items = document.querySelectorAll('#items-list ');
+
+//     items.forEach(function(item) {
+//         if (item.textContent.toLowerCase().includes(filter)) {
+//             item.style.display = '';
+//         } else {
+//             item.style.display = 'none';
+//         }
+//     }
+// });
